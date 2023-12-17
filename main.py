269d5afbd2501b9266e216ac27622d08a29df67e
xiaoxinmiao <xiaoxm_001@outlook.com>
2023-12-17 13:21:40 +0800

    添加工作日志

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import csv
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from jira import JIRA
 from traceback import format_exc
+from copy import deepcopy
+
+import pytz
 
 server = "https://reddate123.atlassian.net"
 options = {
@@ -30,13 +34,25 @@ jira = JIRA(
 
 # Get all projects
 projects = jira.projects()
-# projects = [projects[13]]
+
+for i, project in enumerate(projects):
+    print(f"{i} {project.key}   {project.name}")
+
+projects = [i for i in projects if i.key == "TEST2"]
+pass
 
 
 def remove_blank(raw, blank_mark, replace_value):
     if (not raw) or raw == blank_mark:
         return replace_value
     return raw
+
+
+def download(project):
+    try:
+        download_issues(project)
+    except Exception as e:
+        print(format_exc())
 
 
 # Download issues for each project
@@ -69,13 +85,21 @@ def download_issues(project):
             "project_name",
             "解决时间",  # resolutiondate
             "创建时间",  # created
+            "描述",  # summary
             "迭代ID",  # customfield_10020_id
             "迭代名称",  # customfield_10020_name
             "迭代状态",  # customfield_10020_state
             "迭代目标",  # customfield_10020_goal
             "迭代开始时间",  # customfield_10020_startDate
             "迭代结束时间",  # customfield_10020_endDate
-            "描述",  # summary
+            "工作日志创建者",
+            "工作日志更新者",
+            "花费时间",
+            "日志",
+            "工作日志id",
+            "工作日志issueid",
+            "创建时间",
+            "更新时间",
         ]
 
         writer.writerow(headers)
@@ -89,12 +113,8 @@ def download_issues(project):
             parent_fields_status = remove_blank(parent_fields_status, "{}", {})
             parent_fields_priority = parent_fields.get("priority", {})
             parent_fields_priority = remove_blank(parent_fields_priority, "{}", {})
-            parent_fields_issuetype = parent_fields.get("issuetype", {})
-            parent_fields_issuetype = remove_blank(parent_fields_issuetype, "{}", {})
             resolution = issue_fields.get("resolution", {})
             resolution = remove_blank(resolution, "{}", {})
-            customfield_10107 = issue_fields.get("customfield_10107", {})
-            customfield_10107 = remove_blank(customfield_10107, "{}", {})
 
             customfield_10102s = issue_fields.get("customfield_10102", [])
             customfield_10102s = remove_blank(customfield_10102s, "[]", [])
@@ -103,13 +123,6 @@ def download_issues(project):
                 if isinstance(customfield_10102s, dict)
                 else ""
             )
-
-            labels = issue_fields.get("labels", 0)
-            labels = remove_blank(labels, "[]", 0)
-
-            timeestimate = issue_fields.get("timeestimate", 0)
-            timeestimate = remove_blank(timeestimate, "[]", 0)
-
             aggregatetimeoriginalestimate = issue_fields.get(
                 "aggregatetimeoriginalestimate", 0
             )
@@ -117,84 +130,120 @@ def download_issues(project):
                 remove_blank(aggregatetimeoriginalestimate, "[]", 0)
             )
 
-            assignee = issue_fields.get("assignee", {})
-            assignee = remove_blank(assignee, "{}", {})
-            status = issue_fields.get("status", {})
-            status = remove_blank(status, "{}", {})
-            customfield_10058 = issue_fields.get("customfield_10058", 0)
-            customfield_10058 = remove_blank(customfield_10058, "[]", 0)
-            customfield_10058 = (
-                customfield_10058
-                if customfield_10058 and (not isinstance(customfield_10058, dict))
-                else 0
-            )
-
-            customfield_10043 = issue_fields.get("customfield_10043", {})
-            customfield_10043 = remove_blank(customfield_10043, "{}", {})
-            customfield_10044 = issue_fields.get("customfield_10044", {})
-            customfield_10044 = remove_blank(customfield_10044, "{}", {})
-            aggregateprogress = issue_fields.get("aggregateprogress", {})
-            aggregateprogress = remove_blank(aggregateprogress, "{}", {})
-            progress = issue_fields.get("progress", {})
-            progress = remove_blank(progress, "{}", {})
-            issuetype = issue_fields.get("issuetype", {})
-            issuetype = remove_blank(issuetype, "{}", {})
-            project = issue_fields.get("project", {})
-            project = remove_blank(project, "{}", {})
             aggregatetimespent = issue_fields.get("aggregatetimespent", 0)
             aggregatetimespent = to_hour(remove_blank(aggregatetimespent, "{}", 0))
+            assignee = issue_fields.get("assignee", {})
+            assignee = remove_blank(assignee, "{}", {})
+
+            status = issue_fields.get("status", {})
+            status = remove_blank(status, "{}", {})
+
+            issuetype = issue_fields.get("issuetype", {})
+            issuetype = remove_blank(issuetype, "{}", {})
+
+            project = issue_fields.get("project", {})
+            project = remove_blank(project, "{}", {})
 
             customfield_10020s = issue_fields.get("customfield_10020", [])
             customfield_10020s = remove_blank(customfield_10020s, "[]", [])
-            customfield_10014 = issue_fields.get("customfield_10014", {})
-            customfield_10014 = remove_blank(customfield_10014, "{}", {})
-            customfield_10015 = issue_fields.get("customfield_10015", {})
-            customfield_10015 = remove_blank(customfield_10015, "{}", {})
-            timetracking = issue_fields.get("timetracking", {})
-            timetracking = remove_blank(timetracking, "{}", {})
-            customfield_10000 = issue_fields.get("customfield_10000", {})
-            customfield_10000 = remove_blank(customfield_10000, "{}", {})
-            customfield_10089 = issue_fields.get("customfield_10089", {})
-            customfield_10089 = remove_blank(customfield_10089, "{}", {})
-            customfield_10122 = issue_fields.get("customfield_10122", {})
-            customfield_10122 = remove_blank(customfield_10122, "{}", {})
-            customfield_10001 = issue_fields.get("customfield_10001", {})
-            customfield_10001 = remove_blank(customfield_10001, "{}", {})
-            for customfield_10020 in customfield_10020s:
-                row = [
-                    issue_fields.get("statuscategorychangedate", ""),
-                    parent.get("id", ""),
-                    parent.get("key", ""),
-                    parent_fields.get("summary", ""),
-                    parent_fields_status.get("name", ""),
-                    parent_fields_priority.get("name", ""),
-                    resolution.get("name", ""),
-                    customfield_10102,
-                    aggregatetimeoriginalestimate,
-                    aggregatetimespent,
-                    assignee.get("displayName", ""),
-                    assignee.get("active", ""),
-                    status.get("name", ""),
-                    issuetype.get("name", ""),
-                    project.get("name"),
-                    issue_fields.get("resolutiondate", ""),
-                    issue_fields.get("created", ""),
-                    customfield_10020.get("id", 0),
-                    customfield_10020.get("name", ""),
-                    customfield_10020.get("state", ""),
-                    customfield_10020.get("goal", ""),
-                    customfield_10020.get("startDate", ""),
-                    customfield_10020.get("endDate", ""),
-                    issue_fields.get("summary", ""),
-                ]
+
+            worklog_top = issue_fields.get("worklog", {})
+            worklog_top = remove_blank(worklog_top, "{}", {})
+
+            worklogs = worklog_top.get("worklogs", [])
+            worklogs = remove_blank(worklogs, "[]", [])
+
+            row_part = [
+                f_date(issue_fields.get("statuscategorychangedate", "")),
+                parent.get("id", ""),
+                parent.get("key", ""),
+                parent_fields.get("summary", ""),
+                parent_fields_status.get("name", ""),
+                parent_fields_priority.get("name", ""),
+                resolution.get("name", ""),
+                customfield_10102,
+                aggregatetimeoriginalestimate,
+                aggregatetimespent,
+                assignee.get("displayName", ""),
+                assignee.get("active", ""),
+                status.get("name", ""),
+                issuetype.get("name", ""),
+                project.get("name"),
+                f_date(issue_fields.get("resolutiondate", "")),
+                f_date(issue_fields.get("created", "")),
+                issue_fields.get("summary", ""),
+            ]
+
+            merge_rows = merge_list(customfield_10020s, worklogs)
+            for i, j in merge_rows:
+                row = deepcopy(row_part)
+                row.append(i.get("id", 0))
+                row.append(i.get("name", ""))
+                row.append(i.get("state", ""))
+                row.append(i.get("goal", ""))
+                row.append(f_date(i.get("startDate", "")))
+                row.append(f_date(i.get("endDate", "")))
+
+                row.append(j.get("author", {}).get("displayName", ""))
+                row.append(j.get("updateAuthor", {}).get("displayName", ""))
+                row.append(to_hour(j.get("timeSpentSeconds", "")))
+                row.append(j.get("comment", ""))
+                row.append(j.get("id", ""))
+                row.append(j.get("issueId", ""))
+                row.append(f_date(j.get("created", "")))
+                row.append(f_date(j.get("updated", "")))
+
                 writer.writerow(row)
+
+
+def f_date(original_time: str) -> str:
+    # original_time = "2023-12-13T17:33:46.976+0800"
+    try:
+        if not original_time:
+            return ""
+        parsed_time = datetime.strptime(original_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+        china_tz = pytz.timezone("Asia/Shanghai")
+        china_time = parsed_time.astimezone(china_tz)
+        return china_time.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception as e:
+        print(4, e)
+        return ""
+
+
+def merge_list(list1, list2):
+    if not list1:
+        list1 = [
+            {
+                "id": 0,
+                "name": "",
+                "state": "",
+                "goal": "",
+                "startDate": "",
+                "endDate": "",
+            }
+        ]
+    if not list2:
+        list2 = [
+            {
+                "author": {},
+                "updateAuthor": {},
+                "timeSpentSeconds": "",
+                "comment": "",
+                "id": 0,
+                "issueId": "",
+                "created": "",
+                "updated": "",
+            }
+        ]
+    rows = []
+    for i in list1:
+        for j in list2:
+            rows.append([i, j])
+    return rows
 
 
 with ThreadPoolExecutor() as executor:
     for project in projects:
-        try:
-            executor.submit(download_issues, project)
-        except Exception as e:
-            print(format_exc())
+        executor.submit(download, project)
 
 print("All issues downloaded successfully!")
---
+++ project.md
@@ -0,0 +1,53 @@
+0 DDCJCWLXM   BSN-DDC基础网络项目
+1 SPARTAN   BSN-Spartan
+2 BSNYGPT   BSN云管平台2.1项目
+3 BSNYGPT2   BSN云管平台2.1项目-文档评审验收
+4 BSNYGPT3   BSN云管平台3.0项目
+5 SFLXT   BSN分布式实名认证服务
+6 GFZWXM   BSN官方专网项目
+7 SIGMA   CloudSigma项目
+8 DNSISFBE   Dedicated Node service in SBP (FISCO BCOS enhancement) (售後)
+9 EBPT   Enterprise BSN PoC –Towngas (香港中華煤氣有限公司)
+10 MED   MediConCen Limited(医结)-售后服务
+11 ML   MediConCen Limited(医结项目)
+12 POC11   POC#11: Multiple Wholesale CBDC Settlement Platform
+13 POC8   POC#8: Digital Asset Tokenization
+14 TEST   TEST
+15 TEST2   TEST2
+16 AV2   专网2.1.3英文版
+17 YWXQ   业务需求池
+18 CPXQC   产品需求池
+19 DNS   全网分布式域名服务（DNS）
+20 BSNDDC   分布式DDC项目
+21 FBSWL   分布式网络
+22 HNZGW   区块链服务网络（BSN）河南省主干网建设项目（一期）
+23 BSN   华北电力大学BSN加速卡性能测试项目
+24 NYL   南阳大数据局区块链项目
+25 KXQKL   可信区块链运行监测服务平台
+26 SJXM   商机项目池
+27 LDQK   宁波区块链一期项目
+28 NBZW2   宁波区块链二期项目
+29 MBYA   宁波区块链项目（售后服务）
+30 JCUX   广州区块链中的数字广东
+31 POC10   数字货币项目-POC#10
+32 FXRJ   斯巴达监控应用
+33 VUPC   新疆塔城项目
+34 JXJ   江西BSN主干网及九江区块链基础设施建设项目
+35 HNLPOC   河南BSN区块链云管平台项目（POC）
+36 VDYM   河南主干网项目（售后服务）
+37 SSSSS   测试项目示例 Scrum 项目
+38 SSLV   海南区块链
+39 IYQS   海南区块链专网项目（售后服务）
+40 CBT   深圳跨境贸易区块链专网项目
+41 MBFM   湖南长沙区块链项目
+42 FNXQ   甘肃区块链专网项目（售后服务）
+43 SCWT   生产问题池
+44 WDBXM   稳定币项目
+45 VIQA   锦州区块链专网项目（售后服务）
+46 NCCH   长安华科开放联盟链托管服务项目（售后服务）
+47 XMGLBWD   项目管理部文档
+48 OC   香港OCGIO共享区块链平台项目 (售後)
+49 XGZW   香港OGCIO共享区块链平台项目二期
+50 EBC   香港城市大學（Enterprise BSN - CityU.）
+51 DI2022   黑龙江区块链专网项目（2022）
+52 AFQG   黑龙江数字政府项目
